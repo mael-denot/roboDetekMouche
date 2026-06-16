4ffe236d44373db3a8f0e459dfb819b047d85426
PadjokeJ <jonatan@padjokej.dev>
2026-06-16 14:16:19 +0200

    feat: LE ROBOT KI DETEK LES MOUCHES

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Mouche detekted");
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting command bot...");
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "Detek a mouche")]
+    Mouche,
+    #[command(description = "Eske l'abeille est contamnée par les pecticide")]
+    Abeille,
+    #[command(description = "On en parle plus que les gens ils peuvent manger les vers de terre")]
+    VerDeTerre,
+    #[command(description = "Detek"]
+    Detek,
+    #[command(description = "Ayamouche")]
+    Ayamouche,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Mouche => {
+            bot.send_message(msg.chat.id, format!("🪰 MOUCHE DETEKTED")).await?
+        }
+        Command::Abeille => {
+            bot.send_message(msg.chat.id, format!("🐝 ABEILLE SUR LE PECTICIDE")).await?
+        }
+        Command::Detek => {
+            bot.send_message(msg.chat.id, format!("🚨 DETEK DETEK DETEK")).await?
+        }
+        Command::VerDeTerre => {
+            bot.send_message(msg.chat.id, format!("😨 On n'en parle plus 🤫🤫 mais les gens 🧑‍🤝‍🧑 ils peuvent ⚡ manger les vers 🪱 de terre")).await?
+        }
+        Command::Ayamouche => {
+            bot.send_message(msg.chat.id, format!("😆"))
+        }
+    };
+
+    Ok(())
 }
